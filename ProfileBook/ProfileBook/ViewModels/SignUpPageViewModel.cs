33620c89ce61c1423ea7ee514fe9258aca4905ac
Stas Krivosheya <stask49@gmail.com>
2021-02-22 23:41:50 +0200

    Performed SignUp inputs validation using StringValidator

--- ProfileBook/ProfileBook/ViewModels/SignUpPageViewModel.cs
+++ ProfileBook/ProfileBook/ViewModels/SignUpPageViewModel.cs
@@ -3,7 +3,9 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Acr.UserDialogs;
 using Prism.Navigation;
+using ProfileBook.Validators;
 
 namespace ProfileBook.ViewModels
 {
@@ -14,6 +16,10 @@ namespace ProfileBook.ViewModels
         {
             Title = "Users SignUp";
         }
+
+        private DelegateCommand _signUpCommand;
+        public DelegateCommand SignUpCommand =>
+            _signUpCommand ?? (_signUpCommand = new DelegateCommand(ExecuteSignUpCommand));
 
         private bool _isButtonEnabled;
         public bool IsEnabled
@@ -65,6 +71,42 @@ namespace ProfileBook.ViewModels
                                        !string.IsNullOrEmpty(_confirmPassword);
 
             IsEnabled = allEntriesAreFilled;
+        }
+
+        private void ExecuteSignUpCommand()
+        {
+            if (Password == ConfirmPassword)
+            {
+                var loginValid = StringValidator.Validate(Login, StringValidator.Login);
+                var passwordValid = StringValidator.Validate(Password, StringValidator.Password);
+
+                if (!loginValid)
+                {
+                    UserDialogs.Instance.Alert("First letter must be non-digit.\n" +
+                                               "Login length must be from 4 to 16 characters", "Invalid login!");
+                }
+                else if (!passwordValid)
+                {
+                    UserDialogs.Instance.Alert("Use at least 1 digit.\n" +
+                                               "Use at least 1 lowercase letter.\n" +
+                                               "Use at least 1 uppercase letter.\n" +
+                                               "Password length must be from 8 to 16 characters.", "Invalid password!");
+                    Password = ConfirmPassword = "";
+                }
+                else
+                {
+                    UserDialogs.Instance.Alert("Everything is ok so far. Wait for implementing repository", "Nice job");
+                }
+
+                // todo: check if login is already taken
+
+                // todo: save to db and navigate to main list view
+
+            }
+            else
+            {
+                UserDialogs.Instance.Alert("Passwords are different!");
+            }
         }
     }
 }
